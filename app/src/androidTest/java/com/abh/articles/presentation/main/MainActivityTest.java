fbe38ecbefbcb4f0be56b982f72d6479f28709e5
NADISHR\a.hamza <Nadi@12345678>
2022-07-31 23:02:00 +0400

    added codcov

---
+++ app/src/androidTest/java/com/abh/articles/presentation/main/MainActivityTest.java
@@ -0,0 +1,223 @@
+package com.abh.articles.presentation.main;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.abh.articles.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(12500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar_tool_bar),
+                                        3),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(4300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            Thread.sleep(2250);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2250);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialTextView = onView(
+                allOf(withId(androidx.appcompat.R.id.title), withText("7 days"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(androidx.appcompat.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction overflowMenuButton4 = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar_tool_bar),
+                                        3),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton4.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2250);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialTextView2 = onView(
+                allOf(withId(androidx.appcompat.R.id.title), withText("30 days"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(androidx.appcompat.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction overflowMenuButton5 = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar_tool_bar),
+                                        3),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton5.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2250);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialTextView3 = onView(
+                allOf(withId(androidx.appcompat.R.id.title), withText("1 day"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(androidx.appcompat.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView3.perform(click());
+/*
+        ViewInteraction recyclerView = onView(
+                allOf(withId(androidx.appcompat.R.id.recycler_view_articles),
+                        childAtPosition(
+                                withId(R.id.container),
+                                1)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));*/
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+       /* ViewInteraction appCompatImageView = onView(
+                allOf(withId(R.id.img_back),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar_tool_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_layout),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageView.perform(click());*/
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
---
+++ app/src/test/java/com/abh/articles/MainViewModulelTest.kt
@@ -0,0 +1,47 @@
+package com.abh.articles
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.abh.articles.data.network.CoroutineDispatcherProvider
+import com.abh.articles.data.networking.repository.ArticleRepositoryImpl
+import com.abh.articles.domain.usecase.GetArticles
+import com.abh.articles.presentation.main.MainViewModel
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.util.concurrent.TimeUnit
+
+class MainViewModulelTest {
+    @get:Rule
+    val mainCoroutineRule = CoroutineRuleConfig()
+
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private lateinit var getArticles: GetArticles
+    private lateinit var viewModel: MainViewModel
+
+    private var articleRepositoryImpl = mock<ArticleRepositoryImpl>()
+
+    @Before
+    fun setup() {
+
+        getArticles = GetArticles(articleRepositoryImpl, CoroutineDispatcherProvider())
+    }
+
+    @Test
+    fun checkViewMOdelInit() = runBlocking {
+
+
+
+        viewModel = MainViewModel(getArticles)
+
+        TimeUnit.MINUTES.sleep(1);
+
+        Assert.assertEquals(false, viewModel.showProgressBar.value)
+    }
+}
